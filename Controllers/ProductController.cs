3c5b1f67b191519fd2148968c40687f492f4c05d
spiridonov_ta <spiridonov_ta@albank.ru>
2022-07-12 18:12:51 +0900

    Add css style

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
-using SportsStore.Models.ViewModels;
+using SportsStore.Models.ViewModels; 
 
 namespace SportsStore.Controllers
 {
