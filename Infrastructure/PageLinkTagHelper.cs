30b516aadc52773320fe54b00df7717902563e41
Tusjuljerk <tuskulspiridonov@mail.ru>
2022-07-13 01:52:05 +0900

    add another bootstrap version, add MVC structure stopped in 267-th page

--- Infrastructure/PageLinkTagHelper.cs
+++ Infrastructure/PageLinkTagHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using SportsStore.Models.ViewModels;
+using System.Collections.Generic;
 
 namespace SportsStore.Infrastructure
 {
@@ -21,8 +22,10 @@ namespace SportsStore.Infrastructure
         public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>();
         public bool PageClassesEnabled { get; set; } = false;
-        public string  PageClass { get; set; }
+        public string PageClass { get; set; }
         public string PageClassNormal   { get; set; }
         public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
@@ -32,6 +35,8 @@ namespace SportsStore.Infrastructure
             for (int i = 0; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
+                PageUrlValues["productPage"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
                 if (PageClassesEnabled)
                 {
